c609d3e0dde27989f93488a7e90b454bd2a65a80
kunaltyagi16 <tyagikunal63@gmail.com>
2023-06-26 11:14:40 +0530

    Captcha and pass visibility added

--- script.js
+++ script.js
@@ -2,6 +2,40 @@ const wrapper = document.querySelector(".wrapper");
 const loginLink = document.querySelector(".login-link");
 const regLink = document.querySelector(".register-link");
 const btnClick = document.querySelector(".pop");
+const eyeicon = document.getElementById("eyeicon")
+const password = document.getElementById("password")
+const eyeicon2 = document.getElementById("eyeicon2")
+const password2 = document.getElementById("password2")
+
+
+eyeicon.onclick=function()
+{
+    if(password.type == "password")
+    {
+    password.type = "text";
+    eyeicon.src="images/eye.svg"
+    }
+    else
+    {
+        password.type = "password"
+        eyeicon.src="images/eye-off.svg"
+    }
+}
+
+eyeicon2.onclick=function()
+{
+    if(password2.type == "password")
+    {
+    password2.type = "text";
+    eyeicon2.src="images/eye.svg"
+    }
+    else
+    {
+        password2.type = "password"
+        eyeicon2.src="images/eye-off.svg"
+    }
+}
+
 
 regLink.addEventListener('click',()=>{
  wrapper.classList.add('active')
